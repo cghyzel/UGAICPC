5544f0093e870e4ec47b7dab0edc84ea84c805e2
Frank Hu <frankhu@uga.edu>
2014-05-31 19:48:14 -0400

    FIRST EVER ACCEPTED ANSWER.
    .1337 BAC QUASI-MAGIC CONFIRMED

---
+++ 4867_Maximum_Square/Frank_Hu.cpp
@@ -0,0 +1,58 @@
+/*{
+ * MaximumSquare.cpp
+ *
+ *  Created on: May 27, 2014
+ *      Author: Frank Hu
+ */
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+int row, col;
+int square[1000][1000];
+int board[1000][1000];
+
+int main() {
+	bool s = true;
+	cin >> row;
+	cin >> col;
+	while (((row) && (col))) {
+
+		int maxLen = 0;
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				cin >> square[i][j];
+			}
+
+		}
+
+		int largestSquareSize = 0;
+		for (int i = row - 1; i >= 0; i--) {
+			for (int j = col - 1; j >= 0; j--) {
+
+				if (square[i][j] == 1) {
+					if (j == col - 1 || i == row - 1) {
+						board[i][j] = 1;
+					} else {
+						int s = min(board[i + 1][j], board[i][j + 1]);
+						int k = min(s, board[i + 1][j + 1]);
+						board[i][j] = k + 1;
+					}
+					if (board[i][j] > largestSquareSize) {
+						largestSquareSize = board[i][j];
+					}
+				} else
+					board[i][j] = 0;
+
+			}
+		}
+
+		cout << largestSquareSize;
+		cout << "\n";
+		cin >> row;
+		cin >> col;
+	}
+}

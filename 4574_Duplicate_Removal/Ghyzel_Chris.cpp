8e4e4481ea056673c713f6b2fac71835a0919ae0
Archos67 <ghyzel.chris@gmail.com>
2014-06-10 17:00:54 -0400

    simplified duplicate removal

--- 4574_Duplicate_Removal/Ghyzel_Chris.cpp
+++ 4574_Duplicate_Removal/Ghyzel_Chris.cpp
@@ -7,7 +7,7 @@ using namespace std;
  * Author: Christopher Ghyzel
  */
 int main() {
-  int n = 0, count = 0, i=0, temp = 0, previous = 0;
+  int n = 0, i=0, temp = 0, previous = 0;
   int ordered [25] = {0};
   
   cin >> n; 
@@ -17,15 +17,11 @@ int main() {
     for(i = 0; i < n; i++) {
       cin >> temp;
       if(temp != previous) //if temp is not the previous
-	ordered[count++] = temp; //add it to the ordered list
+	cout << temp << " ";
       previous = temp;
     }
-    // print guesses in original order with no duplicates
-    for(i = 0; i < count; i++) {
-      cout << ordered[i] << " ";
-    }
+
     cout << "$" << endl; //end of output
-    count = 0;
     cin >> n;
   }
 }

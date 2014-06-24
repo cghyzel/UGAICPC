ead4345b51b2dc63b98b35e1ff5a581a0713267e
Frank Hu <frankhu@uga.edu>
2014-06-24 18:35:16 -0400

    Update and rename damerau_levenshtein to damerau_levenshtein.cpp

--- general/damerau_levenshtein.cpp
+++ general/damerau_levenshtein.cpp
@@ -7,6 +7,7 @@ using namespace std;
 // This is comparing the two words, word1 and word 2, and the array d_L_Matrix
 
 int main(){
+  // Damerau Levenshtein Matrix
   int ** d_L_Matrix;
   char * word1;
   char * word2;
@@ -23,12 +24,32 @@ int main(){
     d_L_Matrix = int[word2Length];
   }
   for(int i =0; i< word1Length;++i){
-  d_L_Matrix[0][i] = 0;
+    d_L_Matrix[0][i] = i;
   }
   for(int i =0 ;i < word2Length; ++i){
-  d_L_Matrix[i][0] = 0;
+    d_L_Matrix[i][0] = i;
   }
-  
-  
+  int cost = 0;
+  for(int i =0; i< word1Length; ++i){
+    for(int j =0 ;j< word2Length; ++j){
+    // If the letter in the word at word1[i] is the same as word[j] we will set the cos to be 0, if not then 2,
+    // In terms of the problem spell checker, icpc, in the regular implementation they use 1,
+      if(word1[i] == word[j]){
+        cost = 0;
+      }else{
+        cost = 2;
+      }
+      
+      
+      d_L_Matrix[i][j] = minimum(d_L_Matrix[i-1][j] + 1 // THis is a deletion mistake
+                                ,
+      
+      
+      
+      
+      
+      
+    }
+  }
 
 }

8f71714a00181db425eaea46709b59cb4fe11f29
Frank Hu <frankhu@uga.edu>
2014-06-24 18:43:55 -0400

    Finished Damerau_Levenshtein

--- general/damerau_levenshtein.cpp
+++ general/damerau_levenshtein.cpp
@@ -32,24 +32,31 @@ int main(){
   int cost = 0;
   for(int i =0; i< word1Length; ++i){
     for(int j =0 ;j< word2Length; ++j){
-    // If the letter in the word at word1[i] is the same as word[j] we will set the cos to be 0, if not then 2,
-    // In terms of the problem spell checker, icpc, in the regular implementation they use 1,
-      if(word1[i] == word[j]){
-        cost = 0;
-      }else{
-        cost = 2;
+      // If the letter in the word at word1[i] is the same as word[j] we will set the cos to be 0, if not then 2,
+      // In terms of the problem spell checker, icpc, in the regular implementation they use 1,
+        if(word1[i] == word[j]){
+          cost = 0;
+        }else{
+          cost = 2;
+        }
+      
+      
+        d_L_Matrix[i][j] = minimum(d_L_Matrix[i-1][j] + 2, // THis is a deletion mistake
+                                d_L_Matrix[i][j-1] + 2, // THis is the insertion mistake
+                                d_L_Matrix[i-1][j-1] + cost}; // this last comparison is a substitution mistake
+                                
+        if(cost == 0){
+                      
+        }else{
+          cost = 1;
+        }                                }
+        if(i > 1 && j > 1 && word1[i] == word2[j-1] && word1[i-1] == word2[j]){
+          d_L_Matrix[i][j] = minimum(d_L_Matrix[i][j], d_L_Matrix[i-2][j-2] + cost); // Comparison between transposition and the other three mistakes, 
+        }
+      
       }
-      
-      
-      d_L_Matrix[i][j] = minimum(d_L_Matrix[i-1][j] + 1 // THis is a deletion mistake
-                                ,
-      
-      
-      
-      
-      
-      
-    }
   }
-
+ cout << d_L_Matrix[word1Length][word2Length]; // Prints the damerau-Levenshtein distance in accordance to the penalties
+                                                // of spell checker, you can change the penalties to 1 to make it 
+                                                // work in accordance with the regular damerau-Levenshtein Formula
 }
